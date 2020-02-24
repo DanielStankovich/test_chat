100ea3aef74731d1d9a695b2bfdf5f990c1e46a4
DanielStankovich <danielst2150@gmail.com>
2020-02-24 23:19:51 +0200

    Update production15

--- ChatApp/wsgi.py
+++ ChatApp/wsgi.py
@@ -15,4 +15,4 @@ from whitenoise.django import DjangoWhiteNoise
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ChatApp.settings')
 
 application = get_wsgi_application()
-application = DjangoWhiteNoise(ChatApp)
+application = DjangoWhiteNoise(application)
